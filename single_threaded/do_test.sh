aaa01ee4ef6b89636334028782866e7305dc7510
Fio Cattaneo <fio.cattaneo@oracle.com>
2018-09-03 15:21:56 -0700

    fix typo

--- single_threaded/do_test.sh
+++ single_threaded/do_test.sh
@@ -10,7 +10,7 @@ DIGITS="\
 "
 for d in $DIGITS
 do
-	echo $0; `date`: begin: ./mpfr_pi $d ramananujan_1910_opt
+	echo $0: `date`: begin: ./mpfr_pi $d ramananujan_1910_opt
 	./mpfr_pi $d ramananujan_1910_opt
-	echo $0; `date`: end: ./mpfr_pi $d ramananujan_1910_opt
+	echo $0: `date`: end: ./mpfr_pi $d ramananujan_1910_opt
 done
